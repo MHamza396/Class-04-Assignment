81d19ed16b8e7658c92ee6ba45ddbee6dd13322e
MHamza396 <162509142+MHamza396@users.noreply.github.com>
2024-03-24 03:14:44 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,19 @@
+let Percentage = 40
+if (Percentage === 90){
+    console.log("You Are Passed With Grade A Plus");
+}
+else if (Percentage === 80){
+    console.log("You Are Passed With Grade A")
+}
+else if (Percentage === 70){
+    console.log("You Are Passed With Grade B")
+}
+else if (Percentage === 60){
+    console.log("You Are Passed With Grade C")
+}
+else if(Percentage === 50){
+    console.log("You Are Passed With Grade D")
+}
+else{
+    console.log("You Are Failed")
+}
